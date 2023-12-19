f75c37f5a1e497627cb3c30cebed45682ed1c2b8
Xim <xim.everae@gmail.com>
2023-12-19 11:14:09 +0100

    background randomizer finshed

---
+++ static/js/randomlettergenerator.js
@@ -0,0 +1,46 @@
+const imagArrayBackground = [
+    'static/img/logos/Gentse Feesten Logos/campagne-1-G.png',
+    'static/img/logos/Gentse Feesten Logos/campagne-2-E.png',
+    'static/img/logos/Gentse Feesten Logos/campagne-3-N.png',
+    'static/img/logos/Gentse Feesten Logos/campagne-4-T.png',
+    'static/img/logos/Gentse Feesten Logos/campagne-5-S.png',
+    'static/img/logos/Gentse Feesten Logos/campagne-6-E.png',
+];
+
+const imageArrayTop = [
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-1-G.svg',
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-2-E.svg',
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-3-N.svg',
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-4-T.svg',
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-5-S.svg',
+    'static/img/logos/Gentse Feesten Logos/GF-logo-2023-6-E.svg',
+];
+
+function generateRandomNumber(min, max) {
+
+    const randomNumber = Math.random();
+      const scaledNumber = min + randomNumber * (5 - 0 + 1);
+    return Math.floor(scaledNumber);
+}
+const randomResult = generateRandomNumber(0, 5);
+
+const numberChosen = randomResult
+
+let chosenImgTop = imageArrayTop[numberChosen];
+
+let chosenImgTopElement = document.getElementById("gentsefeestenlogo");
+
+chosenImgTopElement.innerHTML = `<img src="${chosenImgTop}">`;
+
+
+let chosenImgBkg = imagArrayBackground[numberChosen];
+
+let chosenImgBkgElement = document.getElementById("mainbackgroundimg");
+
+chosenImgBkgElement.innerHTML = `<img src="${chosenImgBkg}">`;
+
+
+
+
+
+
