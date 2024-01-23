dbe67578dc41eba04c121157ce4616c7e0fb8c46
Xim <xim.everae@gmail.com>
2024-01-23 19:22:53 +0100

    2de last push

--- static/js/events.js
+++ static/js/events.js
@@ -3,14 +3,15 @@ function fetchEvents(eventsurl){
     .then((response) => {
         return response.json()
     })
+    
     .then((events) => {
         console.log(events)
     let eventContainer = document.querySelectorAll(".events > div");
     for (let number = 0; number < eventContainer.length; number++){
-        eventContainer[number].innerHTML = events[number].title;
+        eventContainer[number].innerHTML = `<div class="eventpicture"><img  src="${events[number].image.thumb}" alt=""></div><p>${events[number].title}</p>`    
     }
     })
 }
 
-let eventsurl = "https://www.pgm.gent/data/gentsefeesten/events_500.json"
+let eventsurl = "https://www.pgm.gent/data/gentsefeesten/events.json"
 fetchEvents(eventsurl)
